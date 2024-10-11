e05f1b28beb737844cc1e740d1d9bbddd5169d5f
lenovo <omkar.reddy211@gmailcom>
2024-10-11 19:07:34 +0530

    payment script

--- dispatch.sh
+++ dispatch.sh
@@ -6,16 +6,7 @@ ececho -e "$color add application user$no_color"
 dnf install golang -y
 
 
-echo -e "$color install golang$no_color"
-useradd roboshop
-echo -e "$color create application directory$no_color"
-rm -rf /app
-mkdir /app
-echo -e "$color download application content$no_color"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
-cd /app
-echo -e "$color extract application content$no_color"
-unzip /tmp/dispatch.zip
+app_prerequisites
 
 echo -e "$color copy download application dependncies$no_color"
 cd /app
