0202455b684459386d72bc762e632b941491c6d3
lenovo <omkar.reddy211@gmailcom>
2024-10-11 16:45:54 +0530

    catalogue script

--- dispatch.sh
+++ dispatch.sh
@@ -1,31 +1,32 @@
-
-echo -e "\e[31mcopy dispatch sevice file\e[0m"
+color=\e[31m
+no_color=\e[0m
+echo -e "$color copy dispatch sevice file$no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
-echo -e "\e[31m install golang\e[0m"
+echo -e "\$color install golang$no_color"
 dnf install golang -y
 
-echo -e "\e[31madd application user\e[0m"
+echo -e "$color add application user$no_color"
 useradd roboshop
 
-echo -e "\e[31mcreate application directory\e[0m"
+echo -e "$color create application directory$no_color"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[31mdownload application content\e[0m"
+echo -e "$color download application content$no_color"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
 cd /app
 
-echo -e "\e[31mextract application content\e[0m"
+echo -e "$color extract application content$no_color"
 unzip /tmp/dispatch.zip
 
-echo -e "\e[31mcopy download application dependncies\e[0m"
+echo -e "$color copy download application dependncies$no_color"
 cd /app
 go mod init dispatch
 go get
 go build
 
-echo -e "\e[31mstart application sevice \e[0m"
+echo -e "$color start application sevice$no_color"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl restart dispatch
