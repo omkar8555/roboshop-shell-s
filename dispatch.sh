acdbc48fe292b8889b223c36474c16e45fea8987
lenovo <omkar.reddy211@gmailcom>
2024-10-11 15:44:06 +0530

    catalogue script

--- dispatch.sh
+++ dispatch.sh
@@ -1,18 +1,30 @@
+
+echo -e "\e[31mcopy dispatch sevice file"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
+echo -e "\e[31m install golang"
 dnf install golang -y
+
+echo -e "\e[31madd application user"
 useradd roboshop
 
+echo -e "\e[31mcreate application directory"
 mkdir /app
+
+echo -e "\e[31mdownload application content"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
 cd /app
+
+echo -e "\e[31mextract application content"
 unzip /tmp/dispatch.zip
 
+echo -e "\e[31mcopy download application dependncies"
 cd /app
 go mod init dispatch
 go get
 go build
 
+echo -e "\e[31mstart application sevice "
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl restart dispatch
