e1795d5e570fd96a354fe37f480e812e6f6ddb83
lenovo <omkar.reddy211@gmailcom>
2024-10-11 19:06:46 +0530

    payment script

--- common.sh
+++ common.sh
@@ -1,2 +1,18 @@
 color="\e[32m"
 no_color="\e[0m"
+
+app_name=dispatch
+
+
+app_prerequisites()  {
+  echo -e "$color install golang$no_color"
+  useradd roboshop
+  echo -e "$color create application directory$no_color"
+  rm -rf /app
+  mkdir /app
+  echo -e "$color download application content$no_color"
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+  cd /app
+  echo -e "$color extract application content$no_color"
+  unzip /tmp/$app_name.zip
+}
--- dispatch.sh
+++ dispatch.sh
@@ -2,20 +2,18 @@ source common.sh
 echo -e "$color copy dispatch service file$no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
-echo -e "$color install golang$no_color"
+ececho -e "$color add application user$no_color"
 dnf install golang -y
 
-echo -e "$color add application user$no_color"
-useradd roboshop
 
+echo -e "$color install golang$no_color"
+useradd roboshop
 echo -e "$color create application directory$no_color"
 rm -rf /app
 mkdir /app
-
 echo -e "$color download application content$no_color"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
 cd /app
-
 echo -e "$color extract application content$no_color"
 unzip /tmp/dispatch.zip
 
