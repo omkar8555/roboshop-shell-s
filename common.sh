794a3f67e34755b6301f96aad25ebdf1ab0a4a84
lenovo <omkar.reddy211@gmailcom>
2024-10-12 18:40:26 +0530

    payment script

--- common.sh
+++ common.sh
@@ -34,7 +34,7 @@ print_heading() {
 
 status_check() {
   if [ $1 -eq 0 ]; then
-    echo  -e "\e[32m success \e[0m"
+    echo  -e "\e[33m success \e[0m"
 else
     echo  -e "\e[31m fail \e[0m"
   fi
