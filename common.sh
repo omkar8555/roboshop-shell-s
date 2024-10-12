7f6c8fa316bf5572c36cff467e381c404008cb1d
lenovo <omkar.reddy211@gmailcom>
2024-10-12 12:39:42 +0530

    payment script

--- common.sh
+++ common.sh
@@ -16,7 +16,7 @@ app_prerequisites()  {
   echo -e "$color download application content$no_color"
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
   cd /app
-  echo -e "$color extract application content$no_color
+  echo -e "$color extract application content$no_color"
   unzip /tmp/$app_name.zip
 }
 
