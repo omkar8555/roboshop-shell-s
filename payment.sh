9e521e34b9c7780f1b41455afbebfa8fd64f8518
lenovo <omkar.reddy211@gmailcom>
2024-10-12 10:13:33 +0530

    payment script

--- payment.sh
+++ payment.sh
@@ -3,16 +3,23 @@ app_name=payment
 
 echo -e "$color copy payment service file$no_color"
 cp payment.service /etc/systemd/system/payment.service
+echo $?
 
 echo -e "$color install python3 file $no_color"
 dnf install python3 gcc python3-devel -y
+echo $?
+
 
 app_prerequisites
 
 cd /app
 pip3 install -r requirements.txt
+echo $?
+
+
 
 echo -e "$color start application sevice$no_color"
 systemctl daemon-reload
 systemctl enable payment
 systemctl restart payment
+echo $?
