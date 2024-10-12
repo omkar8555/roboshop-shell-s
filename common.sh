02d77cc85faad9fc7f2b33242fcb221263732623
lenovo <omkar.reddy211@gmailcom>
2024-10-12 12:38:18 +0530

    payment script

--- common.sh
+++ common.sh
@@ -9,14 +9,14 @@ rm -f $log-file
 
 app_prerequisites()  {
   echo -e "$color add application user$no_color"
-  useradd roboshop &>>$log_file
+  useradd roboshop
   echo -e "$color create application directory$no_color"
   rm -rf /app
-  mkdir /app &>>$log_file
+  mkdir /app
   echo -e "$color download application content$no_color"
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
   cd /app
-  echo -e "$color extract application content$no_color &>>$log_file
+  echo -e "$color extract application content$no_color
   unzip /tmp/$app_name.zip
 }
 
