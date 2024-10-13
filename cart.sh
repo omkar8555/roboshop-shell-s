88ee10cc4f22f235e4cefb1a5bbbb0d443b3070b
lenovo <omkar.reddy211@gmailcom>
2024-10-13 15:31:54 +0530

    payment script

--- cart.sh
+++ cart.sh
@@ -1,21 +1,21 @@
 source common.sh
 
-print_heading1 "copy cart service "
-cp cart.service  /etc/systemd/system/cart.service &>>$log_file1
-status_check1 $?
-dnf module disable nodejs -y &>>$log_file1
-dnf module enable nodejs:20 -y &>>$log_file1
+print_heading "copy cart service "
+cp cart.service  /etc/systemd/system/cart.service &>>$log_file
+status_check $?
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:20 -y &>>$log_file
 
-print_heading1 "install nodejs "
-dnf install nodejs -y &>>$log_file1
-status_check1 $?
+print_heading "install nodejs "
+dnf install nodejs -y &>>$log_file
+status_check $?
 
-pre_reqiuisites1
+pre_reqiuisites
 
-cd /app &>>$log_file1
-npm install &>>$log_file1
-print_heading1 "start application services "
-systemctl daemon-reload &>>$log_file1
-systemctl enable cart &>>$log_file1
-systemctl restart cart &>>$log_file1
-status_check1 $?
+cd /app &>>$log_file
+npm install &>>$log_file
+print_heading "start application services "
+systemctl daemon-reload &>>$log_file
+systemctl enable cart &>>$log_file
+systemctl restart cart &>>$log_file
+status_check $?
--- common.sh
+++ common.sh
@@ -9,7 +9,11 @@ rm -f $log-file
 
 app_prerequisites()  {
   print_heading add application user
-  useradd roboshop &>>$log_file
+ id roboshop &>>$log_file
+ if [ $? -ne 0 ]; then
+     useradd roboshop &>>$log_file
+   fi
+
   status_check $?
   print_heading create application directory
   rm -rf /app &>>$log_file
@@ -39,42 +43,5 @@ else
     echo  -e "\e[31m fail \e[0m"
   fi
 }
---------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------
-pre_reqiuisites1() {
-  print_heading1 "add application user"
-id roboshop &>>$log_file1
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>$log_file1
-  fi
 
-  status_check1 $?
-  rm -rf /app &>>$log_file1
-  print_heading1 "create application directory  "
-  mkdir /app &>>$log_file1
-  status_check1 $?
-  print_heading1 "download application content "
-  curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$log_file1
-  status_check1 $?
-  cd /app
-  print_heading1 "extract application content "
-  unzip /tmp/cart.zip &>>$log_file1
-  status_check1 $?
-}
 
-log_file1=/tmp/roboshop.log
-rm -rf $log_file1
-
-print_heading1() {
-  echo -e "$color $1 $no_color" &>>$log_file1
-  echo -e "$color $1 $no_color"
-
-}
-
-status_check1() {
-  if [ $1 -eq 0 ]; then
-    echo  -e "\e[33m success \e[0m"
-else
-    echo  -e "\e[31m fail \e[0m"
-  fi
-}
