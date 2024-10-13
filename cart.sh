270dce0edb0c66797ec784ce15443d1af1b969bd
lenovo <omkar.reddy211@gmailcom>
2024-10-13 15:37:31 +0530

    payment script

--- cart.sh
+++ cart.sh
@@ -1,12 +1,12 @@
 source common.sh
 
-print_heading "copy cart service "
+print_heading "copy cart service"
 cp cart.service  /etc/systemd/system/cart.service &>>$log_file
 status_check $?
 dnf module disable nodejs -y &>>$log_file
 dnf module enable nodejs:20 -y &>>$log_file
 
-print_heading "install nodejs "
+print_heading "install nodejs"
 dnf install nodejs -y &>>$log_file
 status_check $?
 
@@ -14,7 +14,7 @@ app_prerequisites
 
 cd /app &>>$log_file
 npm install &>>$log_file
-print_heading "start application services "
+print_heading "start application services"
 systemctl daemon-reload &>>$log_file
 systemctl enable cart &>>$log_file
 systemctl restart cart &>>$log_file
