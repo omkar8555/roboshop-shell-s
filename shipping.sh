52ef3c4eea444976a78ed1dcb22f3631a53b3869
lenovo <omkar.reddy211@gmailcom>
2024-10-13 18:25:51 +0530

    payment script

--- shipping.sh
+++ shipping.sh
@@ -9,7 +9,7 @@ dnf install maven -y &>>log_file
 app_prerequisites
 
 mvn clean package &>>log_file
-mv target/shipping-1.0.jar $add_name.jar &>>log_file
+mv target/$add_name-1.0.jar $add_name.jar &>>log_file
 dnf install mysql -y &>>log_file
 status_check $?
 
@@ -17,7 +17,7 @@ for sql_file in schema app-user master-data; do
 mysql -h mysql.rdevopsb72.shop -uroot -pRoboShop@1 < /app/db/$sql_file.sql
 status_check $?
 done
-status_check $?
+
 systemctl daemon-reload &>>log_file
 systemctl enable shipping &>>log_file
 systemctl restart shipping &>>log_file
