af418a6477b2b763ac6e1f66796bb5ebb5949c7a
lenovo <omkar.reddy211@gmailcom>
2024-10-13 21:50:30 +0530

    payment script

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,18 @@
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
-dnf install mongodb-org -y
+source common.sh
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+print_heading "copy mongodb repo file"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>log_file
+status_check $?
 
-systemctl enable mongod
-systemctl restart mongod
+print_heading "install mongodb"
+dnf install mongodb-org -y &>>log_file
+status_check $?
+
+print_heading "update mongodb listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>log_file
+status_check $?
+
+print_heading "start mongodb service"
+systemctl enable mongod &>>log_file
+systemctl restart mongod &>>log_file
+status_check $?
