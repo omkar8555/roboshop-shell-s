c095bc1406e2750045c21e505eb6df4f050262ed
lenovo <omkar.reddy211@gmailcom>
2024-10-13 22:17:41 +0530

    payment script

--- user.sh
+++ user.sh
@@ -1,11 +1,6 @@
 source common.sh
+app_name=user
 
-print_heading "copy user services"
-cp user.service /etc/systemd/system/user.service &>>log_file
-status_check $?
 
 nodejs_setup
 
-print_heading "restart user"
-systemctl restart user &>>log_file
-status_check $?
