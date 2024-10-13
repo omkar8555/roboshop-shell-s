58d45e5346d9a11bb1fa4a8f0ce954a45746f832
lenovo <omkar.reddy211@gmailcom>
2024-10-13 22:30:46 +0530

    payment script

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,13 @@
-dnf install mysql-server -y
 
-systemctl enable mysqld
-systemctl start mysqld
+print_heading "install mysql"
+dnf install mysql-server -y &>>log_file
+status_check $?
 
-mysql_secure_installation --set-root-pass RoboShop@1
+print_heading " start mysql sevice"
+systemctl enable mysqld &>>log_file
+systemctl start mysqld &>>log_file
+status_check $?
+
+print_heading "set mysql password"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>log_file
+status_check $?
