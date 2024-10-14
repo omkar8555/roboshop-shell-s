16fa2da65dc44cf7b9e52c7cb9d983d1083b6814
lenovo <omkar.reddy211@gmailcom>
2024-10-14 14:14:45 +0530

    payment script

--- catalogue.sh
+++ catalogue.sh
@@ -3,18 +3,18 @@ app_name=catalogue
 
 nodejs_setup
 
-print_heading "copy mangodb repo file"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>log_file
+print_heading "Copy MongoDB repo file"
+cp $scripts_path/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
 status_check $?
 
-print_heading "install mongodb clint"
-dnf install mongodb-mongosh -y &>>log_file
+print_heading "Install MongoDB Client"
+dnf install mongodb-mongosh -y &>>$log_file
 status_check $?
 
-print_heading "load master data"
-mongosh --host mongodb.rdevopsb72.shop </app/db/master-data.js &>>log_file
+print_heading "Load Master Data"
+mongosh --host mongodb.rdevopsb72.shop </app/db/master-data.js  &>>$log_file
 status_check $?
 
-print_heading "restart catalogue service"
-systemctl restart catalogue &>>log_file
+print_heading "Restart Catalogue Service"
+systemctl restart catalogue &>>$log_file
 status_check $?
