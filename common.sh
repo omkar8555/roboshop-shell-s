402e9823daa9013d9be4f5d896be0e4c09099a0b
lenovo <omkar.reddy211@gmailcom>
2024-10-14 15:19:35 +0530

    payment script

--- common.sh
+++ common.sh
@@ -111,3 +111,21 @@ python_setup() {
 
 
 }
+
+golang_setup() {
+  print_heading "install golang"
+  dnf install golang -y &>>$log_file
+  status_check $?
+
+  app_prerequisites
+
+  print_heading "copy download application dependncies"
+  cd /app
+  go mod init dispatch  &>>$log_file
+  go get  &>>$log_file
+  go build &>>$log_file
+  status_check $?
+
+  systemd_setup
+
+}
--- dispatch.sh
+++ dispatch.sh
@@ -1,29 +1,22 @@
 source common.sh
 app_name=dispatch
 
-print_heading "copy dispatch service file"
-cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
-status_check $?
+
+if [ -z "$1" ]; then
+  echo INput RABBITMQ_PASSWORD is missing
+  exit 1
+fi
+
+RABBITMQ_PASSWORD=$1
+
+golang_setup
 
 
-print_heading "install golang"
-dnf install golang -y &>>$log_file
-status_check $?
 
 
 
-app_prerequisites
-
-print_heading "copy download application dependncies"
-cd /app
-go mod init dispatch  &>>$log_file
-go get  &>>$log_file
-go build &>>$log_file
-status_check $?
 
 
-print_heading "start application sevice"
-systemctl daemon-reload &>>$log_file
-systemctl enable dispatch &>>$log_file
-systemctl restart dispatch &>>$log_file
-status_check $?
+
+
+
