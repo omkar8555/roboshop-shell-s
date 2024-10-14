653b162db986cd0ba27f3e77daf1aaa97110ffdd
lenovo <omkar.reddy211@gmailcom>
2024-10-14 11:41:14 +0530

    payment script

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,10 @@
 source common.sh
+
+if [-z "$1"]; then
+  echo input mysql password is missing
+  exit 1
+  fi
+
 print_heading "install mysql"
 dnf install mysql-server -y &>>log_file
 status_check $?
@@ -9,5 +15,5 @@ systemctl start mysqld &>>log_file
 status_check $?
 
 print_heading "set mysql password"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>log_file
+mysql_secure_installation --set-root-pass $1 &>>log_file
 status_check $?
