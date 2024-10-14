65743313bcf931077544199f748a925a36a84421
lenovo <omkar.reddy211@gmailcom>
2024-10-14 15:08:41 +0530

    payment script

--- payment.sh
+++ payment.sh
@@ -1,9 +1,7 @@
 source  common.sh
 app_name=payment
 
-print_heading " copy payment service file"
-cp payment.service /etc/systemd/system/payment.service &>>log_file
-status_check $?
+
 
 if [ -z "$1" ]; then
   echo INput RABBITMQ_PASSWORD is missing
@@ -16,6 +14,4 @@ python_setup
 
 
 
-print_heading " load application sevice "
-systemctl restart payment &>>log_file
-status_check$?
+
