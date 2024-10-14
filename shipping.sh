3efd1128307ba03e07fcf381ce29de48fc2151f3
lenovo <omkar.reddy211@gmailcom>
2024-10-14 12:19:13 +0530

    payment script

--- shipping.sh
+++ shipping.sh
@@ -1,13 +1,20 @@
 source common.sh
 app_name=shipping
 
+ if [ -z "$1" ]; then
+   echo INput MySQL Root Password is missing
+      exit 1
+    fi
+
+    MYSQL_ROOT_PASSWORD=$1
+
 
 maven_setup
 
 
 
   for sql_file in schema app-user master-data; do
-mysql -h mysql.rdevopsb72.shop -uroot -pRoboShop@1 < /app/db/$sql_file.sql
+mysql -h mysql.rdevopsb72.shop -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/$sql_file.sql
 status_check $?
 done
 
