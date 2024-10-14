3be2ce3a5d189b9409a651591e4d72e764d77978
lenovo <omkar.reddy211@gmailcom>
2024-10-14 11:58:43 +0530

    payment script

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,12 @@
 source common.sh
 app_name=mysql
 
+ if [ -z "$1" ]; then
+   echo INput MySQL Root Password is missing
+   exit 1
+ fi
+
+ MYSQL_ROOT_PASSWORD=$1
 
 
 print_heading "Install MySQL Server"
@@ -13,5 +19,5 @@ systemctl start mysqld &>>$log_file
 status_check $?
 
 print_heading "Setup MySQL Password"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>$log_file
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$log_file
 status_check $?
