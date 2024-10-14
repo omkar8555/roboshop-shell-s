7294d60f83d5903f8f45524bcf689067a4065544
lenovo <omkar.reddy211@gmailcom>
2024-10-14 09:46:22 +0530

    payment script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,18 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+print_heading "Copy RabbitMQ Repo file"
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>log_file
+status_check $?
+
+print_heading "install rabbitmq server"
+dnf install rabbitmq-server -y  &>>log_file
+status_check $?
+
+print_heading "start rabbitmq sever"
+systemctl enable rabbitmq-server &>>log_file
+systemctl start rabbitmq-server  &>>log_file
+status_check $?
+
+print_heading "Add Application User"
+rabbitmqctl add_user roboshop roboshop123 &>>log_file
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>log_file
+status_check $?
